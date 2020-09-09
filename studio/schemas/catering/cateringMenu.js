b01103e3710b46129fda647a1b6632140631ea8e
Viktor Sjöblom <54740341+ViktorSjoblom@users.noreply.github.com>
2020-09-09 17:03:13 +0200

    started to style

---
+++ studio/schemas/catering/cateringMenu.js
@@ -0,0 +1,28 @@
+export default {
+  name: "cateringMenu",
+  title: "Catering Meny",
+  type: "object",
+  fields: [
+    {
+      name: "menuTitle",
+      title: "Titel för meny",
+      type: "string",
+    },
+    {
+      name: "One",
+      title: "Första",
+      type: "string",
+    },
+
+    {
+      name: "Two",
+      title: "Andra",
+      type: "string",
+    },
+    {
+      name: "Three",
+      title: "Tredje",
+      type: "string",
+    },
+  ],
+};
