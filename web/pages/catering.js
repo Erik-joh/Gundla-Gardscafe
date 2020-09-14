b656abfa4a960b6985914b6b70cf352299b8a5fc
Viktor Sjöblom <Viktorsjoblom93@gmail.com>
2020-09-14 20:07:26 +0200

    restruction

--- web/pages/catering.js
+++ web/pages/catering.js
@@ -1,5 +1,6 @@
 import styles from "../styles/catering.module.css";
 import client from "../client";
+import urlFor from "../imageBuilder";
 import Layout from "../components/layout";
 import CateringGroup from "../components/catering/cateringGroup";
 import CateringHeader from "../components/catering/cateringHeader";
@@ -10,70 +11,67 @@ import Form from "../components/catering/booking.js";
 
 export default function Catering(props) {
   return (
-
     <Layout menu={props.menu}>
+      <div>
+        <div className={styles.cateringWrapper}>
+          {props.catering.cateringHeader.map((item) => {
+            return (
+              <CateringHeader
+                image={item.headerImage}
+                title={item.title}
+                subTitle={item.subTitle}
+                description={item.description}
+                key={item._key}
+              />
+            );
+          })}
 
+          {props.catering.contentImages.map((item) => {
+            return (
+              <CateringGroup
+                topLeftImage={item.topLeftImage}
+                menu={props.catering.cateringMenu[0]}
+                topRightImage={item.topRightImage}
+                bottomLeftImage={item.bottomLeftImage}
+                bottomRightImage={item.bottomRightImage}
+                key={item._key}
+              />
+            );
+          })}
 
-    <div>
-      <div className={styles.cateringWrapper}>
-        {props.cateringHeader.map((item) => {
-          return (
-            <CateringHeader
-              image={item.headerImage}
-              title={item.title}
-              subTitle={item.subTitle}
-              description={item.description}
-              key={item._key}
-            />
-          );
-        })}
+          {props.catering.cateringInfo.map((item) => {
+            console.log(item);
+            return (
+              <Form
+                telephoneNumber={item.telephoneNumber}
+                email={item.email}
+                key={item._key}
+              />
+            );
+          })}
 
-        {props.contentImages.map((item) => {
-          return (
-            <CateringGroup
-              topLeftImage={item.topLeftImage}
-              menu={props.cateringMenu[0]}
-              topRightImage={item.topRightImage}
-              bottomLeftImage={item.bottomLeftImage}
-              bottomRightImage={item.bottomRightImage}
-              key={item._key}
-            />
-          );
-        })}
-
-        {props.cateringInfo.map((item) => {
-          console.log(item);
-          return (
-            <Form
-              telephoneNumber={item.telephoneNumber}
-              email={item.email}
-              key={item._key}
-            />
-          );
-        })}
-
-        {props.quoteSlideshow.map((item) => {
-          return (
-            <QuoteCarousel
-              quoteOne={item.slideQuoteOne}
-              quoteTwo={item.slideQuoteTwo}
-              quoteThree={item.slideQuoteThree}
-              key={item._key}
-            />
-          );
-        })}
-        {props.imageSlideshow.map((item) => {
-          console.log(item);
-          return (
-            <Swipers
-              imageOne={urlFor(item.slideImageOne).url()}
-              imageTwo={urlFor(item.slideImageTwo).url()}
-              imageThree={urlFor(item.slideImageThree).url()}
-              key={item._key}
-            />
-          );
-        })}
-
+          {props.catering.quoteSlideshow.map((item) => {
+            return (
+              <QuoteCarousel
+                quoteOne={item.slideQuoteOne}
+                quoteTwo={item.slideQuoteTwo}
+                quoteThree={item.slideQuoteThree}
+                key={item._key}
+              />
+            );
+          })}
+          {props.catering.imageSlideshow.map((item) => {
+            console.log(item);
+            return (
+              <Swipers
+                imageOne={urlFor(item.slideImageOne).url()}
+                imageTwo={urlFor(item.slideImageTwo).url()}
+                imageThree={urlFor(item.slideImageThree).url()}
+                key={item._key}
+              />
+            );
+          })}
+        </div>
       </div>
     </Layout>
   );
@@ -85,12 +83,12 @@ export async function getStaticProps() {
   const catering = await client.fetch(
     `
     *[_type == "catering"][0]
-  `
+    `
   );
   const menu = await client.fetch(
     `
-    *[_type == "menu"][0]
-  `
+      *[_type == "menu"][0]
+      `
   );
   // const posts = await res.json();
   // By returning { props: posts }, the Blog component
