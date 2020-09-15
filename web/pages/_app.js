cddc9cc8a8b272bfcb9440ffc56de96cdc6c5eba
Erik-joh <38989132+Erik-joh@users.noreply.github.com>
2020-09-15 17:28:06 +0200

    Update _app.js

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,11 +1,10 @@
 import "../styles/globals.css";
-<<<<<<< Updated upstream
-=======
+
 import "swiper/swiper.scss";
 import Navigation from "swiper/components/navigation/navigation.scss";
 import Pagination from "swiper/components/pagination/pagination.scss";
 import Scrollbar from "swiper/components/scrollbar/scrollbar.scss";
->>>>>>> Stashed changes
+
 
 import "../styles/globals.css";
 
